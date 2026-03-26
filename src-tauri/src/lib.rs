d299c895b2dc15382047e88ed0a4538c763de014
零泠丶 <632177447@qq.com>
2026-03-26 10:10:33 +0800

    修复 Tauri 更新插件初始化

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -116,7 +116,7 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_updater::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
         .invoke_handler(tauri::generate_handler![
             generate_context, 
